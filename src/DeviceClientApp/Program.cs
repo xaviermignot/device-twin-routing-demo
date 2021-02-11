bbe2a21861d13c225fe07c369c1656c7eaaa2226
Xavier Mignot <code@xmi.fr>
2021-02-11 23:46:50 +0100

    Single twin change with top level statement

--- src/DeviceClientApp/Program.cs
+++ src/DeviceClientApp/Program.cs
@@ -1,57 +1,32 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Shared;
 using Microsoft.Extensions.Configuration;
 
-namespace DeviceClientApp
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets(Assembly.GetExecutingAssembly())
+    .Build();
+
+var deviceConnectionString = configuration["DeviceConnectionString"];
+
+if (string.IsNullOrEmpty(deviceConnectionString))
 {
-    class Program
-    {
-        private static DeviceClient _deviceClient;
-
-        static async Task Main(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddUserSecrets<Program>()
-                .Build();
-
-            var deviceConnectionString = configuration["DeviceConnectionString"];
-            _deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
-
-            var tokenSource = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, _) =>
-            {
-                Console.WriteLine("Cancellation requested !");
-                tokenSource.Cancel();
-            };
-
-            await StartDeviceAsync(tokenSource.Token);
-        }
-
-        private static async Task StartDeviceAsync(CancellationToken cancellationToken)
-        {
-            var twinCollection = new TwinCollection();
-            twinCollection["LastStartDate"] = DateTime.UtcNow.ToString("s");
-            Console.WriteLine("Updating device last start date...");
-            await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
-
-            var blueColor = false;
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                await UpdateDeviceColorAsync(blueColor ? "Blue" : "Red");
-                blueColor = !blueColor;
-                await Task.Delay(3000);
-            }
-        }
-
-        private static Task UpdateDeviceColorAsync(string color)
-        {
-            var twinCollection = new TwinCollection();
-            twinCollection["Color"] = color;
-            Console.WriteLine($"Set device color to {color}");
-            return _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
-        }
-    }
+    Console.WriteLine("Device connection string not found !");
+    Console.WriteLine("Run dotnet user-secrets set DeviceConnectionString '<DEVICE CONNECTION STRING>'");
+    return;
 }
+
+var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+
+Console.WriteLine("DeviceClient created ! Getting current twin...");
+var currentTwin = await deviceClient.GetTwinAsync();
+var currentColor = currentTwin.Properties.Reported.Contains("color") ? currentTwin.Properties.Reported["color"] : null;
+var newColor = currentColor == "Blue" ? "Red" : "Blue";
+
+var twinUpdate = new TwinCollection();
+twinUpdate["color"] = newColor;
+
+Console.WriteLine($"Changing 'color' reported property from '{currentColor}' to '{newColor}'...");
+await deviceClient.UpdateReportedPropertiesAsync(twinUpdate);
+Console.WriteLine("Done ! Exiting now...");
