93875ef8e7276b7a29229b3aede8c176b477f96b
Xavier Mignot <code@xmi.fr>
2021-02-13 08:29:17 +0100

    Change logic for first run

--- src/DeviceClientApp/Program.cs
+++ src/DeviceClientApp/Program.cs
@@ -17,16 +17,25 @@ if (string.IsNullOrEmpty(deviceConnectionString))
     return;
 }
 
-var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
 
 Console.WriteLine("DeviceClient created ! Getting current twin...");
 var currentTwin = await deviceClient.GetTwinAsync();
-var currentColor = currentTwin.Properties.Reported.Contains("color") ? currentTwin.Properties.Reported["color"] : null;
-var newColor = currentColor == "Blue" ? "Red" : "Blue";
 
 var twinUpdate = new TwinCollection();
-twinUpdate["color"] = newColor;
+if (!currentTwin.Properties.Reported.Contains("color"))
+{
+    Console.WriteLine("First time with this device, let's initialize it !");
+    twinUpdate["propertyUsedOnlyOnce"] = "This is set only once";
+    twinUpdate["color"] = "Red";
+}
+else
+{
+    var currentColor = currentTwin.Properties.Reported["color"];
+    var newColor = currentColor == "Blue" ? "Red" : "Blue";
+    twinUpdate["color"] = newColor;
+    Console.WriteLine($"Changing 'color' reported property from '{currentColor}' to '{newColor}'...");
+}
 
-Console.WriteLine($"Changing 'color' reported property from '{currentColor}' to '{newColor}'...");
 await deviceClient.UpdateReportedPropertiesAsync(twinUpdate);
 Console.WriteLine("Done ! Exiting now...");
